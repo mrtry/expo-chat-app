232dd13dee5a99cf8dca5e49d502c0ea84c3117c
mrtry <yamamoto9302@gmail.com>
2019-09-09 00:01:56 +0900

    create IdentifyRepository

---
+++ src/store/repository/IdentifyRepository.ts
@@ -0,0 +1,14 @@
+import { AsyncStorage } from 'react-native'
+import generateUuid from 'uuid/v1'
+
+const KEY_UUID = 'KEY_UUID'
+
+export async function getUuidOrGenerate(): Promise<string | null> {
+  const result = await AsyncStorage.getItem(KEY_UUID)
+  if (result != null) return Promise.resolve(result)
+
+  const uuid = generateUuid()
+  await AsyncStorage.setItem(KEY_UUID, uuid)
+
+  return Promise.resolve(uuid)
+}
